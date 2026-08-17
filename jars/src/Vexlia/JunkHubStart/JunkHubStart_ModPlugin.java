10518669685fd1c9ec96a50501bec8f34161e9f6
Vexlia-Artemiss <vexlia.artist@gmail.com>
2026-08-17 11:40:42 +0300

    Paintjob Fix

--- jars/src/Vexlia/JunkHubStart/JunkHubStart_ModPlugin.java
+++ jars/src/Vexlia/JunkHubStart/JunkHubStart_ModPlugin.java
@@ -1,21 +1,9 @@
 package Vexlia.JunkHubStart;
 
 import com.fs.starfarer.api.BaseModPlugin;
-import com.fs.starfarer.api.EveryFrameScript;
-import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.campaign.CampaignFleetAPI;
-import com.fs.starfarer.api.combat.ShipVariantAPI;
-import com.fs.starfarer.api.fleet.FleetMemberAPI;
-import com.fs.starfarer.api.impl.campaign.DModManager;
-import com.fs.starfarer.api.impl.campaign.ids.HullMods;
-import com.fs.starfarer.api.impl.campaign.ids.Tags;
+
 import com.fs.starfarer.api.impl.codex.CodexDataV2;
 import com.fs.starfarer.api.impl.codex.CodexEntryPlugin;
-import com.fs.starfarer.api.loading.HullModSpecAPI;
-import com.fs.starfarer.api.loading.VariantSource;
-import com.fs.starfarer.api.util.Misc;
-import exerelin.utilities.NexUtils;
-import lunalib.lunaSettings.LunaSettings;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,9 +11,7 @@ import java.util.Random;
 
 public class JunkHubStart_ModPlugin extends BaseModPlugin {
 
-
     boolean serious_mode = false;
-
 
     @Override
     public void onAboutToLinkCodexEntries() {
@@ -37,8 +23,6 @@ public class JunkHubStart_ModPlugin extends BaseModPlugin {
     private void JHS_CodexLinks(){
         createReciprocalLink(CodexDataV2.getShipEntryId("JHS_angryChungus"), CodexDataV2.getWeaponEntryId("JHS_angryChungus_launcher"));
         createReciprocalLink(CodexDataV2.getShipEntryId("JHS_smallChungus"), CodexDataV2.getWeaponEntryId("JHS_smallChungus_launcher"));
-        //createReciprocalLink(CodexDataV2.getShipEntryId("jdp_grendel_lp"), CodexDataV2.getShipEntryId("grendel"));
-        //createReciprocalLink(CodexDataV2.getShipEntryId("jdp_buffalo_pmmm"), CodexDataV2.getShipEntryId("buffalo"));
     }
 
     private static void createReciprocalLink(String entryIdOne, String entryIdTwo) {
@@ -49,14 +33,14 @@ public class JunkHubStart_ModPlugin extends BaseModPlugin {
         entryTwo.addRelatedEntry(entryOne);
     }
 
-
     @Override
     public void onApplicationLoad() throws Exception {
         super.onApplicationLoad();
+    }
 
-        if (Global.getSettings().getModManager().isModEnabled("lunalib")) {
-            //serious_mode = LunaSettings.getBoolean("Vexlia_FDS", "VFDS_FirstName");
-        }
+
+    @Override
+    public void onGameLoad(boolean newGame) {
 
     }
 
